3331385b8b99a1ac13e3585de1a45f02386880a7
Raphael Halmas Silva <raphaelhalmas@gmail.com>
2020-06-03 17:06:04 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Exercício 01 do Bootcamp GoStack 11 da Rocketseat
+
+O exercício proposto está relacionado à criação de uma API para manutenção de projetos. A API em questão será desenvolvida utilizando Node.js
+
+## Instalação e execução
+
+1. Faça um clone desse repositório;
+2. Entre na pasta rodando `cd bootcamp-gostack-exercicio-01`;
+3. Rode `yarn` para instalar as dependências;
+4. Rode `yarn dev`.
---
+++ src/index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const { uuid, isUuid } = require('uuidv4');
+
+const app = express();
+const projects = [];
+
+function log(request, response, next) {
+    const { method, url } = request;
+    const label = `[${method.toUpperCase()}] ${url}`;    
+    console.time(label);
+    next();
+    console.timeEnd(label);
+}
+
+function validateProjectId(request, response, next) {
+    const { id } = request.params;
+    
+    if (!isUuid(id)) {
+        return response.status(400).json({ error: 'Invalid project ID.' });    
+    }
+
+    return next();
+}
+
+app.use(express.json());
+app.use(log);
+app.use('/projects/:id', validateProjectId);
+
+app.get('/projects', (request, response) => {
+    const { title } = request.query;
+    const results = title 
+        ? projects.filter(project => project.title.includes(title))
+        : projects;
+
+    return response.json(results);
+});
+
+app.post('/projects', (request, response) => {
+    const { title, owner } = request.body;
+    const project = { id: uuid(), title, owner };
+    projects.push(project);
+    return response.json(project);
+});
+
+app.put('/projects/:id', (request, response) => {
+    const { id } = request.params;
+    const { title, owner } = request.body;    
+    const index = projects.findIndex(project => project.id == id);
+
+    if (index < 0) {
+        return response.status(400).json({ error: 'Project not found.' });
+    }
+
+    const project = {
+        id,
+        title,
+        owner
+    };
+
+    projects[index] = project;
+    return response.json(project);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    const { id } = request.params;
+    const index = projects.findIndex(project => project.id == id);
+
+    if (index < 0) {
+        return response.status(400).json({ error: 'Project not found.' });
+    }    
+
+    projects.splice(index, 1);
+    return response.status(204).send();
+});
+
+app.listen(3333, () => {
+    console.log('Back-end started');
+});
